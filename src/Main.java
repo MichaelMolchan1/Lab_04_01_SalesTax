7798956bcfa8e21c561a258cbe67bf926cd3c1ca
Molch <molchaml@mail.uc.edu>
2022-09-18 23:04:54 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Variables
+        double price = 23.21;
+        double tax = price * 0.05;
+        System.out.println("Your item costs $" + price + " and the tax on that item will be " + tax);
+
+    }
+}
